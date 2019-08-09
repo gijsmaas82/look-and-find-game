c49c1e24d23a790696d45b6eb91ea02df65c7c93
gijsmaas82 <gijsmaas82@outlook.com>
2019-08-09 14:17:19 +0200

    final things are done

--- index.js
+++ index.js
@@ -6,7 +6,28 @@ const images = [{
     src: 'styles/boat2.jpg'
 },{
     name: 'boat3',
-    src: 'styles/boat3.jpg'
+    src: 'styles/boat1.jpg'
+},{
+    name: 'boat4',
+    src: 'styles/boat2.jpg'
+},{
+    name: 'boat5',
+    src: 'styles/boat1.jpg'
+},{
+    name: 'boat6',
+    src: 'styles/boat2.jpg'
+},{
+    name: 'boat7',
+    src: 'styles/boat1.jpg'
+},{
+    name: 'boat8',
+    src: 'styles/boat2.jpg'
+},{
+    name: 'boat9',
+    src: 'styles/boat1.jpg'
+},{
+    name: 'boat10',
+    src: 'styles/boat2.jpg'
 }]
 
 for (let i = 0; i <images.length; i++){
@@ -14,7 +35,7 @@ for (let i = 0; i <images.length; i++){
     const img = document.createElement('img')
     img.alt = images[i].name
     img.src = images[i].src
-    img.style = 'height: 100px; position: absolute; bottom: ' + getRandomInt(0, 30) + 'rem; left: ' + getRandomInt(0, 60) + 'rem'
+    img.style = 'height: 100px; position: absolute; bottom: ' + getRandomInt(10, 30) + 'rem; left: ' + getRandomInt(0, 60) + 'rem'
     img.onclick = function () {
         addToFoundItems(images[i])
     } 
@@ -32,5 +53,34 @@ const foundItems = []
 function addToFoundItems(image) {
     if (!foundItems.includes(image)) {
         foundItems.push(image)
-    } 
+        displayInFooter()
+    }
 }
+
+function displayInFooter() {
+    const foundItemsSection = document.getElementById('foundItems')
+    foundItemsSection.innerHTML = ''
+    for (let i = 0; i < foundItems.length; i++) {
+        const img = document.createElement('img')
+        img.alt = foundItems[i].name
+        img.src = foundItems[i].src
+        img.style.height = '50px'
+        foundItemsSection.appendChild(img)
+    }
+    if (foundItems.length === images.length) {
+        alert('You have won! You found all the boats. Awesome!!')
+    }
+}
+
+function userName() {
+    const player = prompt('Please enter your name')
+    if (player !== '') {
+        const playerItems = document.getElementById('playerName')
+        playerItems.textContent = player + "'s items"
+    } else {
+        const playerItems = document.getElementById('playerName')
+        playerItems.textContent = "player's items"
+    }
+}
+
+userName()  
